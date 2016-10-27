41a19da357a0dbd0d0f42c948cc25768744becf8
Nickmyers217 <nickbmyers217@gmail.com>
2016-10-26 22:55:01 -0500

    Added a test simulation from the quil examples

--- src/nature_of_code_clojure/core.clj
+++ src/nature_of_code_clojure/core.clj
@@ -1,6 +1,18 @@
-(ns nature-of-code-clojure.core)
+(ns nature-of-code-clojure.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m])
+  (:require [nature-of-code-clojure.dynamic :as dynamic]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(q/defsketch hello-quil
+  :title "You spin my circle right round"
+  :size [500 500]
+  ; setup function called only once, during sketch initialization.
+  :setup dynamic/setup
+  ; update-state is called on each iteration before draw-state.
+  :update dynamic/update-state
+  :draw dynamic/draw-state
+  :features [:keep-on-top]
+  ; This sketch uses functional-mode middleware.
+  ; Check quil wiki for more info about middlewares and particularly
+  ; fun-mode.
+  :middleware [m/fun-mode])
