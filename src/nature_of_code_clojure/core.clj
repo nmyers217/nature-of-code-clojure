73f7931e2a188d1108ee48ccab4237869fc784ab
Nickmyers217 <nickbmyers217@gmail.com>
2016-11-03 00:01:08 -0500

    Started working on friction simulation

--- src/nature_of_code_clojure/core.clj
+++ src/nature_of_code_clojure/core.clj
@@ -6,9 +6,10 @@
             [nature-of-code-clojure.bouncing-ball :as bouncing-ball]
             [nature-of-code-clojure.falling-ball :as falling-ball]
             [nature-of-code-clojure.accel-towards-mouse :as atm]
-            [nature-of-code-clojure.gravity-and-wind :as gaw]))
+            [nature-of-code-clojure.gravity-and-wind :as gaw]
+            [nature-of-code-clojure.friction-cube :as fc]))
 
-;;; TODO: One day make this run a gui where you can select a simulation
+;; TODO: One day make this run a gui where you can select a simulation
 (defn -main [& args]
   (println
    (str
@@ -16,7 +17,7 @@
      "=> (use 'nature-of-code-clojure.core)\n"
      "=> (run-[simulation name])")))
 
-(defmacro setup-sketch
+(defmacro defsimulation
   "Constructs and runs a simulation given its meta data"
   [sketch-name title size setup-fn update-fn draw-fn nav?]
   `(q/defsketch ~(symbol sketch-name)
@@ -27,20 +28,17 @@
      ; update-state is called on each iteration before draw-state.
      :update ~update-fn
      :draw   ~draw-fn
-     ; the window will always be on top even when unfocused
-     ; this makes live development easier
-     :features [:keep-on-top]
      ; These sketches use functional-mode middleware.
      ; Check quil wiki for more info about middlewares and particularly
      ; fun-mode.
      :middleware (if ~nav?
-                   [m/fun-mode m/pause-on-error m/navigation-2d]
-                   [m/fun-mode m/pause-on-error])))
+                   [m/fun-mode m/navigation-2d]
+                   [m/fun-mode])))
 
 (defn run-example
   "Runs the example simulation"
   []
-  (setup-sketch
+  (defsimulation
     "example-simulation" "You spin my circle right round"
     [500 500]
     example/setup example/update-state example/draw-state
@@ -49,7 +47,7 @@
 (defn run-random-walker
   "Runs the random walker simulation"
   []
-  (setup-sketch
+  (defsimulation
     "random-walker-simulation" "Random Walker"
     [500 500]
     random-walker/setup random-walker/update-state random-walker/draw-state
@@ -58,7 +56,7 @@
 (defn run-bouncing-ball
   "Runs the bouncing ball simulation"
   []
-  (setup-sketch
+  (defsimulation
     "bouncing-ball-simulation" "Bouncing Ball"
     [500 500]
     bouncing-ball/setup bouncing-ball/update-state bouncing-ball/draw-state
@@ -67,7 +65,7 @@
 (defn run-falling-ball
   "Runs the falling ball simulation"
   []
-  (setup-sketch
+  (defsimulation
     "falling-ball-simulation" "Falling Ball"
     [500 500]
     falling-ball/setup falling-ball/update-state falling-ball/draw-state
@@ -76,7 +74,7 @@
 (defn run-accel-towards-mouse
   "Runs the accel towards mouse simulation"
   []
-  (setup-sketch
+  (defsimulation
     "accel-towards-mouse-simulation" "Accelerate Towards Mouse"
     [1000 1000]
     atm/setup atm/update-state atm/draw-state
@@ -85,8 +83,17 @@
 (defn run-gravity-and-wind
   "Runs the gravity and wind balls simulation"
   []
-  (setup-sketch
+  (defsimulation
    "gravity-and-wind-simulation" "Gravity and Wind"
    [1000 1000]
    gaw/setup gaw/update-state gaw/draw-state
    false))
+
+(defn run-friction-cube
+  "Runs the friction cube simulation"
+  []
+  (defsimulation
+    "friction-cube-simulation" "Friction Cube"
+    [1000 750]
+    fc/setup fc/update-state fc/draw-state
+    false))
---
+++ src/nature_of_code_clojure/friction_cube.clj
@@ -0,0 +1,70 @@
+(ns nature-of-code-clojure.friction-cube
+  (:require [quil.core :as q]))
+
+(defn create-surface
+  "Returns the data for a surface"
+  [friction-amt color rect]
+  {:friction-amt friction-amt
+   :color color
+   :rect rect})
+
+(defn create-world
+  "Return a map containing the surfaces (grass, dirt, and ice) for the game world"
+  []
+  (map #(apply create-surface %)
+       [[0.05 [0   255 0]   [0   500 500 10]]
+        [0.10 [122 100 80]  [500 500 250 10]]
+        [0.01 [100 160 255] [750 500 250 10]]]))
+
+(defn create-player
+  "Return a map containing the player data"
+  []
+  {:position [20 475]
+   :velocity [0.0 0.0]
+   :acceleration [0.0 0.0]
+   :color [255 0 0]
+   :size 25
+   :mass 2
+   :velocity-cap 5})
+
+(defn create-forces
+  "Return a map of force functions"
+  []
+  {:gravity (fn [mass] [0.0 0.01])})
+
+(defn setup
+  "Returns the initial state"
+  []
+  {:world  (create-world)
+   :player (create-player)
+   :forces (create-forces)})
+
+(defn update-state
+  "Updates the world to the next state"
+  [state]
+  state)
+
+(defn draw-surface
+  "Draws a surface"
+  [{:keys [color rect]}]
+  (apply q/fill color)
+  (apply q/rect rect))
+
+(defn draw-world
+  "Draws the world"
+  [world]
+  (doseq [surface world] (draw-surface surface)))
+
+(defn draw-player
+  "Draws a player"
+  [{:keys [color size position]}]
+  (apply q/fill color)
+  (q/rect (first position) (second position) size size))
+
+(defn draw-state
+  "Draws the current state"
+  [state]
+  (q/background 51)
+  (let [{:keys [world player]} state]
+    (draw-world world)
+    (draw-player player)))
