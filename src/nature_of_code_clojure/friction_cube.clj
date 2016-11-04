fa7c59269cfed4f41ccfb428852e8d7133580a2a
Nickmyers217 <nickbmyers217@gmail.com>
2016-11-03 21:46:52 -0500

    Added the ability to move with the keyboard

--- src/nature_of_code_clojure/friction_cube.clj
+++ src/nature_of_code_clojure/friction_cube.clj
@@ -1,6 +1,7 @@
 (ns nature-of-code-clojure.friction-cube
   (:require [quil.core :as q]))
 
+;; Setup functions
 (defn create-surface
   "Returns the data for a surface"
   [friction-amt color rect]
@@ -8,13 +9,20 @@
    :color color
    :rect rect})
 
+(def surface-types
+  "A function to generate each surface type at a given rect"
+  {:grass #(create-surface 0.05 [0 255 0] %)
+   :dirt  #(create-surface 0.10 [122 100 80] %)
+   :ice   #(create-surface 0.01 [100 160 255] %)})
+
 (defn create-world
-  "Return a map containing the surfaces (grass, dirt, and ice) for the game world"
+  "Return a vector of surfaces for the game world"
   []
-  (map #(apply create-surface %)
-       [[0.05 [0   255 0]   [0   500 500 10]]
-        [0.10 [122 100 80]  [500 500 250 10]]
-        [0.01 [100 160 255] [750 500 250 10]]]))
+  (map (fn [{:keys [type rect]}]
+         ((type surface-types) rect))
+       [{:type :grass :rect [0 500 500 10]}
+        {:type :dirt  :rect [500 500 250 10]}
+        {:type :ice   :rect [750 500 250 10]}]))
 
 (defn create-player
   "Return a map containing the player data"
@@ -30,7 +38,7 @@
 (defn create-forces
   "Return a map of force functions"
   []
-  {:gravity (fn [mass] [0.0 0.01])})
+  {:gravity (fn [mass] [0 0])})
 
 (defn setup
   "Returns the initial state"
@@ -39,21 +47,48 @@
    :player (create-player)
    :forces (create-forces)})
 
+
+;; Update functions
+(defn move-player
+  "Adds the player's velocity to their position"
+  [forces player]
+  (let [new-acc (reduce #(map + %1 ((second %2) (:mass player))) [0 0] forces)
+        new-vel (map + (:velocity player) new-acc)
+        new-pos (map + (:position player) new-vel)]
+    (assoc player :acceleration new-acc :velocity new-vel :position new-pos)))
+
 (defn update-state
   "Updates the world to the next state"
   [state]
-  state)
+  (update state :player #(move-player (:forces state) %)))
 
+;; TODO: Make this properly and smoothly accelerate the player
+;; Need a key map in the state to track the pressed keys
+;; The actual updates to anything besides the keymap should take place in update-state
 (defn on-key-down
   "Updates the state when a key is pressed"
   [state event]
-  state)
+  (case (:key event)
+    (:a :left)  (assoc-in state [:forces :thrust]
+                          (fn [mass] (map #(/ % mass) [-0.5 0.0])))
+    (:d :right) (assoc-in state [:forces :thrust]
+                          (fn [mass] (map #(/ % mass) [0.5 0.0])))
+    state))
 
+;; TODO: Make this properly and smoothly accelerate the player
+;; Need a key map in the state to track the pressed keys
+;; The actual updates to anything besides the keymap should take place in update-state
 (defn on-key-up
   "Updates the state when a key is released"
   [state]
-  state)
+  (if (contains? #{:left :right :a :d} (q/key-as-keyword))
+    (-> state
+        (assoc-in [:forces :thrust] (fn [mass] [0 0]))
+        (assoc-in [:player :velocity] [0 0]))
+    state))
 
+
+;; Drawing functions
 (defn draw-surface
   "Draws a surface"
   [{:keys [color rect]}]
